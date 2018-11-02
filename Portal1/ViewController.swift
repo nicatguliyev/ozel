610ee2dc52b5932be57523ba803ed79d217d0398
Nicat Guliyev <nicatguliyev@newht.local>
2018-11-02 11:17:37 +0400

    Initial Commit

---
+++ Portal1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Portal1
+//
+//  Created by Nicat Guliyev on 11/2/18.
+//  Copyright © 2018 Nicat Guliyev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
